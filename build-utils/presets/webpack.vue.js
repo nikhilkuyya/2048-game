e009ecaa76344bc99b6bf7fc103ba7bd2a8d23cb
nikhilkuyya <nikhilkuyya@gmail.com>
2018-11-23 17:37:59 +0530

    build support setup

--- build-utils/presets/webpack.vue.js
+++ build-utils/presets/webpack.vue.js
@@ -1,5 +1,8 @@
 const path = require("path")
 const VueLoaderPlugin = require("vue-loader/lib/plugin")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+
+const isProd = process.env.NODE_ENV === "production"
 
 module.exports = () => {
   return {
@@ -8,10 +11,23 @@ module.exports = () => {
         {
           test: /\.vue$/,
           loader: "vue-loader"
+        },
+        {
+          test: /\.css$/,
+          use: [
+            isProd ? MiniCssExtractPlugin.loader : "vue-style-loader",
+            "css-loader"
+          ]
         }
       ]
     },
-    plugins: [new VueLoaderPlugin()],
+    plugins: [
+      new VueLoaderPlugin(),
+      new MiniCssExtractPlugin({
+        filename: "[name].css",
+        chunkFilename: "[id].css"
+      })
+    ],
     resolve: {
       alias: {
         vue$: path.resolve(
